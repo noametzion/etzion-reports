eca96fe24df9728076e223c86d7b7d7afabfc51c
NoamAlike <noam@alike.health>
2025-06-25 14:36:15 +0300

    split screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+"use client";
+
 import styles from "./page.module.css";
+import ResizableView from "@/app/components/ResizableView";
+import Survey from "@/app/components/SurveysViewer";
+import Reports from "@/app/components/ReportViewer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <ResizableView
+          left={<Survey />}
+          right={<Reports />}
+          defaultSplit={50}
+          minWidth={25}
+        />
       </main>
-      <footer className={styles.footer}>
-      </footer>
     </div>
   );
 }
