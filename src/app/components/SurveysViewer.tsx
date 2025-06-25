55a6959963c88752a8828a61c8b63084ca550a08
NoamAlike <noam@alike.health>
2025-06-25 14:39:01 +0300

    split screen

---
+++ src/app/components/SurveysViewer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './SurveysViewer.module.css';
+
+const SurveysViewer = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Surveys</h2>
+    </div>
+  );
+};
+
+export default SurveysViewer;
